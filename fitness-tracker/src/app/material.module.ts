761652583f43af05d52fbe8c68a47f2ed4952c0e
Hoai Duc Nguyen <hoaiducnew@gmail.com>
2018-04-25 06:30:29 -0400

    add checkbox

--- fitness-tracker/src/app/material.module.ts
+++ fitness-tracker/src/app/material.module.ts
@@ -1,12 +1,14 @@
 import {NgModule} from '@angular/core';
 import {
-  MatButtonModule, MatDatepickerModule, MatFormFieldModule, MatIconModule, MatInputModule,
+  MatButtonModule, MatCheckboxModule, MatDatepickerModule, MatFormFieldModule, MatIconModule, MatInputModule,
   MatNativeDateModule
 } from '@angular/material';
 
 @NgModule({
-  imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule],
-  exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule]
+  imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule,
+    MatDatepickerModule, MatNativeDateModule, MatCheckboxModule],
+  exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule,
+    MatDatepickerModule, MatNativeDateModule, MatCheckboxModule]
 })
 export class MaterialModule {
 }
