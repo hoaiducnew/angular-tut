b2bca0d4de5feef567b83bfa0a200c30545b8ae9
Hoai Duc Nguyen <hoaiducnew@gmail.com>
2018-04-24 21:01:08 -0400

    signup form: - flex layout

--- fitness-tracker/src/app/app.module.ts
+++ fitness-tracker/src/app/app.module.ts
@@ -13,6 +13,8 @@ import {NewTrainingComponent} from './training/new-training/new-training.compone
 import {PastTrainingsComponent} from './training/past-trainings/past-trainings.component';
 import {WelcomeComponent} from './welcome/welcome.component';
 import {AppRoutingModule} from './app-routing.module';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {FormsModule} from '@angular/forms';
 
 
 @NgModule({
@@ -30,7 +32,9 @@ import {AppRoutingModule} from './app-routing.module';
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
